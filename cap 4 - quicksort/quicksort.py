eff602d11f0555c2039adf880b5ce1b3ffba15a3
Daniel Souza <workdndsza@gmail.com>
2024-11-19 16:46:10 -0300

    quicksort - ordenacao rapida de um array

---
+++ cap 4 - quicksort/quicksort.py
@@ -0,0 +1,12 @@
+# Quicksort - ordenação rápida de um array
+
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr 
+    else:
+        pivo = arr[0]
+        menores = [i for i in arr[1:] if i <= pivo]
+        maiores = [i for i in arr[1:] if i > pivo]
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+    
+print(quicksort([10, 5, 2, 3]))
